de57c2190e785b3a02e903d469cbba262d48e79e
Cameron Sharp <sharpcameron@hotmail.co.uk>
2018-01-30 14:02:25 +0000

    Flags now saves file names if applicable

--- wordcount.c
+++ wordcount.c
@@ -2,33 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-void setup(int argc, char **argv) {
-    int use_stdin = 1;
-    int use_stdout = 1;
-    int ignore_case = 0;
+int use_stdin = 1;
+char* file_input;
 
+int use_stdout = 1;
+char* file_output;
+
+int ignore_case = 0;
+
+void setup(int argc, char **argv) {
     for(int i = 0; i < argc; i++) {
         // Decide whether or not to read from stdin
         if (strcmp(argv[i], "-i") == 0) {
             use_stdin = 0;
+            file_input = argv[i + 1];
         }
         // Decide whether or not to output to stdout
-        else if (strcmp(argv[i], "-o") == 0) {
+        if (strcmp(argv[i], "-o") == 0) {
             use_stdout = 0;
+            file_output = argv[i + 1];
         }
         // Decide whether or not to ignore case
-        else if (strcmp(argv[i], "-c") == 0) {
+        if (strcmp(argv[i], "-c") == 0) {
             ignore_case = 1;
         }
     }
-
     printf("\nUse STDIN: %d\nUse STDOUT: %d\nIgnore case: %d\n\n", use_stdin, use_stdout, ignore_case);
+    printf("Input file: %s\nOutput file: %s\n", file_input, file_output);
 }
 
 int main(int argc, char **argv) {
-
     // Run setup
     setup(argc, argv);
+
+    char input[50000];
+
+    if (use_stdin != 0) {
+        fgets(input, 50, stdin);
+        printf(input);
+    } else {
+        printf("test");
+    }
+
 
     return 0;
 }
