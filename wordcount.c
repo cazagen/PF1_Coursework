eb2b775aa6ae0a87bb71c0778d960f63c81b8605
Cameron Sharp <sharpcameron@hotmail.co.uk>
2018-01-30 13:36:50 +0000

    Basics for flags added

--- wordcount.c
+++ wordcount.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
 
@@ -9,13 +10,21 @@ int main(int argc, char **argv) {
 
     int use_stdin = 1;
     int use_stdout = 1;
+    int ignore_case = 0;
 
     for(int i = 0; i < argc; i++) {
-        if (argv[i] == "-o") {
-            printf("-o appeared");
+        if (strcmp(argv[i], "-i") == 0) {
+            use_stdin = 0;
         }
-        printf("-o did not appear");
+        if (strcmp(argv[i], "-o") == 0) {
+            use_stdout = 0;
+        }
+        if (strcmp(argv[i], "-c") == 0) {
+            ignore_case = 1;
+        }
     }
+
+    printf("\nUse STDIN: %d\nUse STDOUT: %d\nIgnore case: %d\n\n", use_stdin, use_stdout, ignore_case);
 
     return 0;
 }
