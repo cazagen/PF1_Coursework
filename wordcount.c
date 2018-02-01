7e6b0a89977519feeee0610ad84a049f07b6f651
Cameron Sharp <sharpcameron@hotmail.co.uk>
2018-02-01 22:16:17 +0000

    Added test files

--- wordcount.c
+++ wordcount.c
@@ -3,10 +3,12 @@
 #include <string.h>
 
 int use_stdin = 1;
-char* file_input;
+char* file_input_name;
+FILE *file_input;
 
 int use_stdout = 1;
-char* file_output;
+char* file_output_name;
+FILE *file_output;
 
 int ignore_case = 0;
 
@@ -15,12 +17,12 @@ void setup(int argc, char **argv) {
         // Decide whether or not to read from stdin
         if (strcmp(argv[i], "-i") == 0) {
             use_stdin = 0;
-            file_input = argv[i + 1];
+            file_input_name = argv[i + 1];
         }
         // Decide whether or not to output to stdout
         if (strcmp(argv[i], "-o") == 0) {
             use_stdout = 0;
-            file_output = argv[i + 1];
+            file_output_name = argv[i + 1];
         }
         // Decide whether or not to ignore case
         if (strcmp(argv[i], "-c") == 0) {
@@ -28,7 +30,7 @@ void setup(int argc, char **argv) {
         }
     }
     printf("\nUse STDIN: %d\nUse STDOUT: %d\nIgnore case: %d\n\n", use_stdin, use_stdout, ignore_case);
-    printf("Input file: %s\nOutput file: %s\n", file_input, file_output);
+    printf("Input file: %s\nOutput file: %s\n\n", file_input_name, file_output_name);
 }
 
 int main(int argc, char **argv) {
@@ -38,12 +40,13 @@ int main(int argc, char **argv) {
     char input[50000];
 
     if (use_stdin != 0) {
-        fgets(input, 50, stdin);
+        fgets(input, 50000, stdin);
         printf(input);
     } else {
-        printf("test");
+        file_input = fopen(file_input_name, "r");
+        fgets(input, 50000, file_input);
+        printf(input);
+        fclose(file_input);
     }
-
-
     return 0;
 }
